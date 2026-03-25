0b695172121232048c992d45f907171f0cfe39aa
Abdallah Khaled <bdallhbdalqadr92@gmail.com>
2026-03-25 19:20:11 +0200

    create project

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const Sammly());
+}
+
+class Sammly extends StatelessWidget {
+  const Sammly({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'SAMMLY AI INTERIOR DESIGN',
+    );
+  }
+}
