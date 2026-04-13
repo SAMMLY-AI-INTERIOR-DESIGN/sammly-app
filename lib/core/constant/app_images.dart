b95be85bbb10d25aaf9b65f4e3ede68be4f24f8f
Abdelrahman-El-Kassas <elkassas380@gmail.com>
2026-04-13 21:29:34 +0200

    feat: add social authentication provider icons and define image constants

--- lib/core/constant/app_images.dart
+++ lib/core/constant/app_images.dart
@@ -1,3 +1,6 @@
-abstract class AppImages {
-  
+class AppImages {
+  static const String splash = "assets/images/logo.svg";
+  static const String facebookicon = "assets/images/facebook.svg";
+  static const String googleicon = "assets/images/google.svg";
+  static const String appleicon = "assets/images/apple.svg";
 }
