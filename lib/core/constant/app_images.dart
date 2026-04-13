3c698ccf1e4a075fc4124ed3f3ff5708a3fee30f
Abdallah Khaled <bdallhbdalqadr92@gmail.com>
2026-04-13 21:32:13 +0200

    test

--- lib/core/constant/app_images.dart
+++ lib/core/constant/app_images.dart
@@ -3,4 +3,5 @@ class AppImages {
   static const String facebookicon = "assets/images/facebook.svg";
   static const String googleicon = "assets/images/google.svg";
   static const String appleicon = "assets/images/apple.svg";
+  static const String nullImape = "";
 }
