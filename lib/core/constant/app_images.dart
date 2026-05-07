528c022623958616ffb7d70cf1f7d2b07be3e258
Abdallah Khaled <bdallhbdalqadr92@gmail.com>
2026-05-07 21:55:47 +0300

    finish favorite ui and add states

--- lib/core/constant/app_images.dart
+++ lib/core/constant/app_images.dart
@@ -7,5 +7,30 @@ class AppImages {
   static const String exploreIcon = "assets/images/explore.svg";
   static const String historyIcon = "assets/images/history.svg";
   static const String profileIcon = "assets/images/profile.svg";
-  static const String successBackground = 'assets/images/success_background.svg';
+  static const String successBackground =
+      'assets/images/success_background.svg';
+  static const String onboarding1 = 'assets/images/onboarding1.png';
+  static const String onboarding2 = 'assets/images/onboarding2.png';
+  static const String onboarding3 = 'assets/images/onboarding3.png';
+  static const String onboardingBack = 'assets/images/onboarding_back.svg';
+  static const String onboardingForward =
+      'assets/images/onboarding_forward.svg';
+  static const String onboardingContainer =
+      'assets/images/onboarding_container.svg';
+  static const String arrowBack = 'assets/images/arrow_back.svg';
+  static const String searchIcon = "assets/images/search.svg";
+  static const String profilePlaceholder =
+      "assets/images/profile_placeholder.svg";
+  static const String aiPoweredIcon = "assets/images/ai_powered.svg";
+  static const String startGenerateIcon = "assets/images/start_generate.svg";
+  static const String placeholderRoomAI = "assets/images/home_placeholder.png";
+  static const String explore1 = "assets/images/explore1.png";
+  static const String explore2 = "assets/images/explore2.png";
+  static const String noNetwork = "assets/images/NoNetwork.png";
+  static const String noResult = "assets/images/NoResult.png";
+  static const String noImage = "assets/images/NoImage.png";
+  static const String noFavorite = "assets/images/NoFavorite.png";
+  static const String noNotification = "assets/images/NoNotifications.png";
+  static const String heartFilled = "assets/images/heartFilled.svg";
+  static const String heartOutline = "assets/images/heartOutline.svg";
 }
