e89d71180dba52a2cd6de08bc0445fab89a788c1
Abdallah Khaled <bdallhbdalqadr92@gmail.com>
2026-06-10 12:52:27 +0300

    solve errors after pull new changes from develop

--- lib/features/home/presentation/views/widgets/home_header.dart
+++ lib/features/home/presentation/views/widgets/home_header.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:sammly/core/constant/app_colors.dart';
+import 'package:sammly/core/constant/app_images.dart';
 import 'package:sammly/core/constant/app_strings.dart';
 import 'package:sammly/core/theme/text_styles.dart';
-import 'package:sammly/core/widgets/avatar_widget.dart';
 import 'package:sammly/features/profile/presentation/cubit/profile_cubit.dart';
 import 'package:sammly/features/profile/presentation/cubit/profile_state.dart';
 import 'package:sammly/features/layout/presentation/cubit/layout_cubit/layout_cubit.dart';
