ce689c30facb31667bc8be3f2a2e82dd3a7e951e
Abdallah Khaled <bdallhbdalqadr92@gmail.com>
2026-06-15 20:37:45 +0300

    display real data of shared designs

--- lib/features/profile/presentation/views/widgets/posts_data_section.dart
+++ lib/features/profile/presentation/views/widgets/posts_data_section.dart
@@ -1,12 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:sammly/core/constant/app_strings.dart';
 import 'package:sammly/core/theme/text_styles.dart';
+import 'package:sammly/core/widgets/custom_heart_item.dart';
 
 class PostsDataSection extends StatelessWidget {
-  const PostsDataSection({super.key});
+  const PostsDataSection({super.key, required this.postsCount, required this.likesCount});
+  final String postsCount;
+  final String likesCount;
 
   @override
   Widget build(BuildContext context) {
-    return Text(AppStrings.sharedImages, style: AppTextStyles.title18SemiBold);
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Text(AppStrings.sharedImages, style: AppTextStyles.title18SemiBold),
+        Row(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Text("$postsCount post", style: AppTextStyles.body14Regular),
+            SizedBox(width: 24.w),
+            Row(
+              children: [
+                const CustomHeartItem(),
+                SizedBox(width: 4.w),
+                Text(likesCount, style: AppTextStyles.body14Regular),
+              ],
+            ),
+          ],
+        ),
+      ],
+    );
   }
 }
--- lib/features/profile/presentation/views/widgets/shared_images_list_view.dart
+++ lib/features/profile/presentation/views/widgets/shared_images_list_view.dart
@@ -1,64 +1,106 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:sammly/core/shared_pref/shared_pref.dart';
+import 'package:sammly/features/Explore/cubit/design_details_cubit.dart';
+import 'package:sammly/features/Explore/cubit/design_details_repo.dart';
+import 'package:sammly/features/Explore/cubit/design_details_states.dart';
 import 'package:sammly/features/profile/data/models/shared_images_model.dart';
 import 'package:sammly/features/profile/presentation/views/widgets/shared_image_card.dart';
 import 'package:sammly/features/History/presentation/views/historydetails.dart';
 
-class SharedImagesListView extends StatelessWidget {
-  SharedImagesListView({super.key});
+class SharedImagesListView extends StatefulWidget {
+  final Function(int postsCount, int likesCount)? onDataCalculated;
+  const SharedImagesListView({super.key, this.onDataCalculated});
 
-  final List<Map<String, dynamic>> sharedData = [
-    {
-      'imageUrl': 'https://images.unsplash.com/photo-1598928506311-c55ded91a20c?q=80&w=600&auto=format&fit=crop',
-      'title': 'Modern Living Room',
-      'likes': 34,
-    },
-    {
-      'imageUrl': 'https://images.unsplash.com/photo-1616046229478-9901c5536a45?q=80&w=600&auto=format&fit=crop',
-      'title': 'Minimalist Bedroom',
-      'likes': 12,
-    },
-    {
-      'imageUrl': 'https://images.unsplash.com/photo-1502672260266-1c1ef2d93688?q=80&w=600&auto=format&fit=crop',
-      'title': 'Cozy Apartment',
-      'likes': 8,
-    },
-    {
-      'imageUrl': 'https://images.unsplash.com/photo-1617104678098-de229db51175?q=80&w=600&auto=format&fit=crop',
-      'title': 'Elegant Kitchen',
-      'likes': 45,
-    },
-  ];
+  @override
+  State<SharedImagesListView> createState() => _SharedImagesListViewState();
+}
+
+class _SharedImagesListViewState extends State<SharedImagesListView> {
+  List<String> _sharedIds = [];
+  final Map<String, int> _likesMap = {};
+
+  @override
+  void initState() {
+    super.initState();
+    _loadSharedIds();
+  }
+
+  void _loadSharedIds() {
+    final idsStr = SharedPref.getData(key: 'shared_design_ids') ?? '';
+    if (idsStr.isNotEmpty) {
+      _sharedIds = idsStr.split(',').where((e) => e.isNotEmpty).toList();
+    }
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      widget.onDataCalculated?.call(_sharedIds.length, 0);
+    });
+  }
+
+  void _updateLikes(String id, int likes) {
+    if (_likesMap[id] != likes) {
+      _likesMap[id] = likes;
+      final totalLikes = _likesMap.values.fold(0, (a, b) => a + b);
+      WidgetsBinding.instance.addPostFrameCallback((_) {
+        widget.onDataCalculated?.call(_sharedIds.length, totalLikes);
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      shrinkWrap: true, 
-      physics: const NeverScrollableScrollPhysics(), 
-      itemCount: sharedData.length, 
-      itemBuilder: (context, index) {
-        final data = sharedData[index];
-        final item = SharedImageModel(
-          title: data['title'],
-          description: "Lorem ipsum dolor sit amet consectetur adipisicing elit",
-          imageUrl: data['imageUrl'],
-          likes: data['likes'],
-        );
+    if (_sharedIds.isEmpty) {
+      return const SizedBox.shrink();
+    }
 
-        return GestureDetector(
-          onTap: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(
-                builder: (context) => HistoryDetailsView(
-                  title: item.title,
-                  imageUrl: item.imageUrl,
-                  designId: 'dummy_id',
-                ),
-              ),
-            );
-          },
-          child: SharedImageCard(
-            item: item,
+    return ListView.builder(
+      shrinkWrap: true,
+      physics: const NeverScrollableScrollPhysics(),
+      itemCount: _sharedIds.length,
+      itemBuilder: (context, index) {
+        final designId = _sharedIds[index];
+
+        return BlocProvider(
+          create: (context) => DesignDetailsCubit(DesignDetailsRepo())..fetchDesignDetails(designId),
+          child: BlocBuilder<DesignDetailsCubit, DesignDetailsState>(
+            builder: (context, state) {
+              if (state is DesignDetailsLoaded) {
+                final design = state.design;
+                final title = '${design.style} design'.trim();
+                
+                _updateLikes(design.id, design.likesCount);
+                
+                final item = SharedImageModel(
+                  title: title.isEmpty ? 'Unknown Design' : title,
+                  description: design.prompt,
+                  imageUrl: design.imageUrl,
+                  likes: design.likesCount,
+                );
+
+                return GestureDetector(
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => HistoryDetailsView(
+                          title: item.title,
+                          imageUrl: item.imageUrl,
+                          designId: design.id,
+                        ),
+                      ),
+                    );
+                  },
+                  child: SharedImageCard(item: item),
+                );
+              } else if (state is DesignDetailsError) {
+                return const SizedBox.shrink();
+              } else {
+                return Padding(
+                  padding: EdgeInsets.symmetric(vertical: 250.h),
+                  child: Center(child: CircularProgressIndicator()),
+                );
+              }
+            },
           ),
         );
       },
